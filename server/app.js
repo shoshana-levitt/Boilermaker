0065d17ff5e0efd7ad35d71efe71d0390687bec0
Shoshana Levitt <shoshana.levitt@gmail.com>
2022-12-20 11:30:39 -0500

    listening on port

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
 
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("*", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../public/index"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("*", function (req, res, next) {
-  res.sendFile(path.join(__dirname, "../public/index"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+module.exports = app;
